3c57bdfcaac73c28a8fd2bca55b48d9d56d4bd53
Ahmet Akgul <akgualhmet@Outlook.com>
2017-02-22 12:01:19 -0500

    Change structure of files.  Tweet button working

--- quote.js
+++ quote.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+  $("#tweet").click(function() {
+    $("#tweet").attr('data-text', 'He said' );
+  });
+
   $("#button").click(function() {
     $.ajax( {
       url: "http://api.forismatic.com/api/1.0/?",
@@ -15,9 +19,10 @@ $(document).ready(function(){
       success: function(data){
         // var post = data.shift();
         $("#quote-source").html("<p>- " + data.quoteAuthor + "</p>");
+        $("#tweet").attr('href', "https://twitter.com/intent/tweet?text=" + formatTweet(data.quoteText, data.quoteAuthor));
         $("#quote").html("<q>"+ data.quoteText + "</q>");
-        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
-          $('#quote-source').html('Source:' + post.custom_meta.Source);
+        if (typeof data.custom_meta !== 'undefined' && typeof data.custom_meta.Source !== 'undefined') {
+          $('#quote-source').html('Source:' + data.custom_meta.Source);
         } else {
           $('#quote-source').text('');
         }
@@ -28,3 +33,8 @@ $(document).ready(function(){
     });
   });
 });
+
+function formatTweet(tweet, author){
+  return "\"" + tweet + "\"" + " - " + author;
+
+}
