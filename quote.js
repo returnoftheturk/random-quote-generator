c135eb89d66341ddb1b96995d8c9a9f99b936e94
Ahmet Akgul <akgualhmet@Outlook.com>
2017-02-22 20:13:05 -0500

    Change color upon button click.  Fade background color out

--- quote.js
+++ quote.js
@@ -4,6 +4,11 @@ $(document).ready(function(){
   });
 
   $("#button").click(function() {
+    console.log(randomColor());
+    // $("body").fadeOut(1000, function(){
+        $("body").css("background-color", randomColor());
+      // });
+      // $("body").animate({backgroundColor: "#ff0000"});
     $.ajax( {
       url: "http://api.forismatic.com/api/1.0/?",
       dataType: "jsonp",
@@ -37,4 +42,11 @@ $(document).ready(function(){
 function formatTweet(tweet, author){
   return "\"" + tweet + "\"" + " - " + author;
 
+}
+var arrayOfColors = ["#1569C7","#3090C7", "#306EFF", "#7FFFD4", "#4EE2EC", "#52D017",
+"#00FF00", "#FFE87C", "#F3E5AB", "#FDD017", "#C68E17", "#966F33", "#C35817", "#E55451", "#E41B17",
+"#7D0552", "#C48189", "#FAAFBA", "#F6358A", "#4B0082", "#842DCE", "#7F38EC", "#FFF5EE"];
+
+function randomColor(){
+  return arrayOfColors[Math.floor(Math.random()*24)];
 }
